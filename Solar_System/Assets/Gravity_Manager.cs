73e517a2eb705b8233d7d55af9a1902b5beb4ff2
HenriqueCL <61242797+TehJaeger@users.noreply.github.com>
2022-05-17 01:53:52 +0100

    changing planets spawn
    Making new prefabs and making it so planets spawn as the game does

--- Solar_System/Assets/Gravity_Manager.cs
+++ Solar_System/Assets/Gravity_Manager.cs
@@ -1,19 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Gravity_Manager : MonoBehaviour
 {
-    float G = 100f;
+    public float G = 100f;
+    public GameObject[] planets;
+
     GameObject[] celestials;
     // Start is called before the first frame update
     void Start()
     {
+        int cont = 0;
+        foreach(GameObject i in planets){
+        switch (cont)
+        {
+            case 1:
+                Instantiate(planets[1],new Vector3(580,0,0),Quaternion.identity);
+                break;
+            case 2:
+                Instantiate(planets[2],new Vector3(1083,0,0),Quaternion.identity);
+                break;
+            case 3:
+                Instantiate(planets[3],new Vector3(1510,0,0),Quaternion.identity);
+                break;
+            case 4:
+                Instantiate(planets[4],new Vector3(1515,0,0),Quaternion.identity);
+                break;
+            case 5:
+                Instantiate(planets[5],new Vector3(2160,0,0),Quaternion.identity);
+                break;
+            case 6:
+                Instantiate(planets[6],new Vector3(7449,0,0),Quaternion.identity);
+                break;
+            case 7:
+                Instantiate(planets[7],new Vector3(14791,0,0),Quaternion.identity);
+                break;
+            case 8:
+                Instantiate(planets[8],new Vector3(29480,0,0),Quaternion.identity);
+                break;
+            case 9:
+                Instantiate(planets[9],new Vector3(44745,0,0),Quaternion.identity);
+                break;
+            default:
+                Instantiate(planets[0],new Vector3(0,0,0),Quaternion.identity);
+                break;
+        }
+        cont++;
+        }
         celestials = GameObject.FindGameObjectsWithTag("Celestial");
         InitialVelocity();
         foreach(GameObject i in celestials){
             i.GetComponent<Rigidbody>().AddTorque(0,1,0,ForceMode.VelocityChange);
         }
+        
     }
     
    private void FixedUpdate() {
